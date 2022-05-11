3450e66648a189ad13a781c9db726b0eb563fb19
Victornrib <victornrib@gmail.com>
2022-05-11 05:17:02 +0200

    Controller to SignInScreenActivity

--- app/src/main/java/com/example/falldetectionapp/controller/SignInController.java
+++ app/src/main/java/com/example/falldetectionapp/controller/SignInController.java
@@ -1,4 +1,66 @@
 package com.example.falldetectionapp.controller;
 
+
+
+import com.example.falldetectionapp.model.SharedPrefs;
+import com.example.falldetectionapp.model.User;
+import com.google.gson.Gson;
+
+
+
 public class SignInController {
+
+    String email;
+    String password;
+    User currentUser;
+
+    private String alertDialogErrorMessage;
+
+
+    public SignInController(String email, String password) {
+
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public boolean checkSignInFields() {
+        currentUser = getUser();
+
+        //Will pass the current user to the 'Singleton'
+
+        return currentUser != null;
+    }
+
+
+    public String getAlertDialogErrorMessage() {
+        return alertDialogErrorMessage;
+    }
+
+
+    private User getUser() {
+
+        Gson gson = new Gson();
+        String jsonRet = SharedPrefs.getString("User", email,null);
+
+        if (jsonRet != null) {
+            currentUser = gson.fromJson(jsonRet, User.class);
+
+            if (currentUser.password.equals(password)) {
+                return currentUser;
+            }
+            else {
+                alertDialogErrorMessage = "Password wrong.";
+                return null;
+            }
+        }
+        else {
+            alertDialogErrorMessage = "Email not found.";
+            return null;
+        }
+    }
+
+
+
+
 }
