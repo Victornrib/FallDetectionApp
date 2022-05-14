5e704c2d97e1e51a8a609a368a1370085a09d8ee
Victornrib <victornrib@gmail.com>
2022-05-14 22:22:10 +0200

    Bluetooth Connection write already working

---
+++ app/src/main/java/com/example/falldetectionapp/view/DeviceListActivity.java
@@ -0,0 +1,69 @@
+package com.example.falldetectionapp.view;
+
+import android.Manifest;
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import java.util.Set;
+
+
+public class DeviceListActivity extends ListActivity {
+
+    private BluetoothAdapter bluetoothAdapter = null;
+    static String MAC_ADDRESS = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> arrayBluetooth = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+            if (pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+                    String deviceName = device.getName();
+                    String deviceMacAddress = device.getAddress();
+                    arrayBluetooth.add(deviceName + "\n" + deviceMacAddress);
+                }
+            }
+            setListAdapter(arrayBluetooth);
+        }
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String generalInformation = ((TextView) v).getText().toString();
+        //Toast.makeText(getApplicationContext(), "Info:\n" + generalInformation, Toast.LENGTH_LONG).show();
+
+        String macAddress = generalInformation.substring(generalInformation.length() - 17);
+        //Toast.makeText(getApplicationContext(), "MAC ADDRESS:\n" + macAddress, Toast.LENGTH_LONG).show();
+
+        Intent returnMacAddress = new Intent();
+        returnMacAddress.putExtra(MAC_ADDRESS, macAddress);
+
+        setResult(RESULT_OK, returnMacAddress);
+        finish();
+    }
+}
--- app/src/main/java/com/example/falldetectionapp/view/PairDeviceScreenActivity.java
+++ app/src/main/java/com/example/falldetectionapp/view/PairDeviceScreenActivity.java
@@ -9,25 +9,45 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.falldetectionapp.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 
 public class PairDeviceScreenActivity extends AppCompatActivity {
     Button buttonBack;
     Button buttonPairDevice;
+    Button buttonTestConnection;
 
-    //BluetoothAdapter bluetoothAdapter;
     private static final int REQUEST_BLUETOOTH_ACTIVATION = 1;
-    BluetoothAdapter bluetoothAdapter;
+    private static final int REQUEST_BLUETOOTH_CONNECTION = 2;
+
+    ConnectedThread connectedThread;
+
+    private static String MAC_ADDRESS = null;
+    UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    BluetoothAdapter bluetoothAdapter = null;
+    BluetoothDevice bluetoothDevice = null;
+    BluetoothSocket bluetoothSocket = null;
+
     AlertDialog alertDialog;
+    boolean connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +69,35 @@ public class PairDeviceScreenActivity extends AppCompatActivity {
         buttonPairDevice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openAddDeviceScreenActivity();
+
+                if (!connected) {
+                    Intent openPairedDevicesList = new Intent(PairDeviceScreenActivity.this, DeviceListActivity.class);
+                    startActivityForResult(openPairedDevicesList, REQUEST_BLUETOOTH_CONNECTION);
+                } else {
+                    try {
+                        Toast.makeText(getApplicationContext(), "Bluetooth disconnected", Toast.LENGTH_LONG).show();
+                        connected = false;
+                        buttonPairDevice.setText("Pair");
+                        bluetoothSocket.close();
+                    } catch (IOException error) {
+                        Toast.makeText(getApplicationContext(), "An error has occurred:\n" + error, Toast.LENGTH_LONG).show();
+                    }
+                }
+                //openAddDeviceScreenActivity();
+            }
+        });
+
+        buttonTestConnection = (Button) findViewById(R.id.buttonTestConnection);
+        buttonTestConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (connected) {
+                    connectedThread.write("Alert");
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Bluetooth is not connected", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -62,7 +110,6 @@ public class PairDeviceScreenActivity extends AppCompatActivity {
     }
 
 
-
     private void startBluetooth() {
 
         //Create alert dialog
@@ -73,24 +120,11 @@ public class PairDeviceScreenActivity extends AppCompatActivity {
             alertDialog.setTitle("Error");
             alertDialog.setMessage("Your device doesn't have bluetooth");
             alertDialog.show();
-        }
-        else {
-            System.out.println("\n\n\n\n\nFOI\n\n\n\n\n");
+        } else {
             Intent activateBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                //
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+                startActivityForResult(activateBluetooth, REQUEST_BLUETOOTH_ACTIVATION);
             }
-            startActivityForResult(activateBluetooth, REQUEST_BLUETOOTH_ACTIVATION);
-            alertDialog.setTitle("Teste");
-            alertDialog.setMessage("Foi ate aqui");
-            alertDialog.show();
         }
 
 
@@ -108,17 +142,97 @@ public class PairDeviceScreenActivity extends AppCompatActivity {
                     alertDialog.setTitle("Success");
                     alertDialog.setMessage("Bluetooth activated");
                     alertDialog.show();
-                }
-                else {
+                } else {
                     alertDialog.setTitle("Error");
                     alertDialog.setMessage("Bluetooth wasn't activated");
                     alertDialog.show();
                 }
                 break;
+
+
+            case REQUEST_BLUETOOTH_CONNECTION:
+                if (resultCode == Activity.RESULT_OK) {
+
+                    MAC_ADDRESS = data.getExtras().getString(DeviceListActivity.MAC_ADDRESS);
+                    //Toast.makeText(getApplicationContext(), "Success obtaining MAC ADDRESS:\n" + MAC_ADDRESS, Toast.LENGTH_LONG).show();
+
+                    bluetoothDevice = bluetoothAdapter.getRemoteDevice(MAC_ADDRESS);
+                    try {
+                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+
+                            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+                            connected = true;
+                            bluetoothSocket.connect();
+                            buttonPairDevice.setText("Unpair");
+
+                            connectedThread = new ConnectedThread(bluetoothSocket);
+                            connectedThread.start();
+
+                            Toast.makeText(getApplicationContext(), "You have been connected with:\n" + MAC_ADDRESS, Toast.LENGTH_LONG).show();
+                        }
+                    } catch (IOException error) {
+                        connected = false;
+                        Toast.makeText(getApplicationContext(), "An error has occurred:\n" + error, Toast.LENGTH_LONG).show();
+                    }
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Didn't receive MAC ADDRESS", Toast.LENGTH_LONG).show();
+                }
         }
     }
 
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private byte[] mmBuffer; // mmBuffer store for the stream
 
+        public ConnectedThread(BluetoothSocket socket) {
+            //bluetoothSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
 
-    //we will need an AlertDialog here to say if connection is successful or not.
+            // Get the input and output streams; using temp objects because
+            // member streams are final.
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {
+
+            }
+            try {
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            mmBuffer = new byte[1024];
+            int numBytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs.
+            while (true) {
+                try {
+                    numBytes = mmInStream.read(mmBuffer);
+
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        // Call this from the main activity to send data to the remote device.
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
 }
