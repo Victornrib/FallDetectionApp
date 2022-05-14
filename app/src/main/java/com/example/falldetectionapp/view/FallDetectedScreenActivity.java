9d05de2fb774dde83a76897d828c53ca0ea7d932
Victornrib <victornrib@gmail.com>
2022-05-14 16:11:19 +0200

    SMS in progress

--- app/src/main/java/com/example/falldetectionapp/view/FallDetectedScreenActivity.java
+++ app/src/main/java/com/example/falldetectionapp/view/FallDetectedScreenActivity.java
@@ -1,12 +1,16 @@
 package com.example.falldetectionapp.view;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -19,11 +23,52 @@ public class FallDetectedScreenActivity extends AppCompatActivity {
 
     private Button buttonSignOut;
     private Button buttonSettings;
-    private String phoneNumber = "28213929";
-    private String message = "you sent an sms!";
+    private String phoneNumber;
+    private String message;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Part of the code that deals with the permissions from the manifest
+        // When this code is uncommented it doesn't give an error message but also doesn't work
+        //You will need to research a bit
+
+        /*
+
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) +
+                ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS))
+                != PackageManager.PERMISSION_GRANTED) {
+
+                // Permission is not granted
+                // Should we show an explanation?
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,"Manifest.permission.READ_SMS") ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this,"Manifest.permission.READ_SMS")) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{"Manifest.permission.READ_SMS, Manifest.permission.SEND_SMS"},
+                        REQUEST_CODE);
+
+                // REQUEST_CODE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        else {
+            // Permission has already been granted
+        }
+
+         */
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fall_detected_screen);
 
@@ -44,6 +89,9 @@ public class FallDetectedScreenActivity extends AppCompatActivity {
                 openSettingsScreenActivity();
             }
         });
+
+        phoneNumber = "91100198";
+        message = "you sent an sms!";
 
         sendSMS(phoneNumber, message);
 
@@ -97,6 +145,8 @@ public class FallDetectedScreenActivity extends AppCompatActivity {
             }
         }, new IntentFilter(SENT));
 
+        /*
+
         // ---when the SMS has been delivered---
         registerReceiver(new BroadcastReceiver() {
             @Override
@@ -114,8 +164,13 @@ public class FallDetectedScreenActivity extends AppCompatActivity {
             }
         }, new IntentFilter(DELIVERED));
 
+        */
+
         SmsManager sms = SmsManager.getDefault();
         sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+
+
+
     }
 
     //not sure if we need this but in theory we need to also close the receiver, maybe in the main activity.
