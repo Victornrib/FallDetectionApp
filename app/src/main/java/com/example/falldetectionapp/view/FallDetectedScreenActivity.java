0a72f551074fdd4abf86fd38584ce88bb398a67c
gho8888 <smgho@ruc.dk>
2022-05-16 10:59:13 +0200

    working sendSMS()

--- app/src/main/java/com/example/falldetectionapp/view/FallDetectedScreenActivity.java
+++ app/src/main/java/com/example/falldetectionapp/view/FallDetectedScreenActivity.java
@@ -1,4 +1,5 @@
 package com.example.falldetectionapp.view;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -23,16 +24,87 @@ public class FallDetectedScreenActivity extends AppCompatActivity {
 
     private Button buttonSignOut;
     private Button buttonSettings;
-    private String phoneNumber;
-    private String message;
-    private static final int REQUEST_CODE = 1;
+    //private String phoneNumber;
+    //private String message;
+    //private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Part of the code that deals with the permissions from the manifest
-        // When this code is uncommented it doesn't give an error message but also doesn't work
-        //You will need to research a bit
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fall_detected_screen);
+
+        //when you click sign out, you go back to initial screen
+        buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openInitialScreenActivity();
+            }
+        });
+
+        //click settings --> settings screen
+        buttonSettings = (Button) findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettingsScreenActivity();
+            }
+        });
+
+        //give permission
+        if (ContextCompat.checkSelfPermission(FallDetectedScreenActivity.this, Manifest.permission.SEND_SMS)
+                == PackageManager.PERMISSION_GRANTED) {
+            sendSMS();
+        } else {
+            ActivityCompat.requestPermissions(FallDetectedScreenActivity.this, new String[]{Manifest.permission.SEND_SMS}, 100);
+        }
+    }
+
+    //code from https://www.youtube.com/watch?v=ofAL1C4jUJw
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if(requestCode == 100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendSMS();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    private void openInitialScreenActivity() {
+        Intent intent = new Intent(this, InitialScreenActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSettingsScreenActivity() {
+        Intent intent = new Intent(this, SettingsScreenActivity.class);
+        startActivity(intent);
+    }
+
+    private void sendSMS() {
+        String phoneNumber = "004528213929";
+        String message = "you sent an sms!";
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+        Toast.makeText(this, "SMS sent", Toast.LENGTH_LONG).show();
+    }
+}
+
+//not sure if we need this but in theory we need to also close the receiver, maybe in the main activity.
+//    @Override
+//    protected void onDestroy() {
+//        unregisterReceiver(receiver);
+//        super.onDestroy();
+//    }
+
+
+
+
+//Part of the code that deals with the permissions from the manifest
+// When this code is uncommented it doesn't give an error message but also doesn't work
+//You will need to research a bit
 
         /*
 
@@ -68,116 +140,66 @@ public class FallDetectedScreenActivity extends AppCompatActivity {
         }
 
          */
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fall_detected_screen);
-
-        //when you click sign out, you go back to initial screen
-        buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
-        buttonSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openInitialScreenActivity();
-            }
-        });
-
-        //click settings --> settings screen
-        buttonSettings = (Button) findViewById(R.id.buttonSettings);
-        buttonSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSettingsScreenActivity();
-            }
-        });
-
-        phoneNumber = "91100198";
-        message = "you sent an sms!";
-
-        sendSMS(phoneNumber, message);
-
-    }
-
-    private void openInitialScreenActivity() {
-        Intent intent = new Intent(this, InitialScreenActivity.class);
-        startActivity(intent);
-    }
-
-    private void openSettingsScreenActivity() {
-        Intent intent = new Intent(this, SettingsScreenActivity.class);
-        startActivity(intent);
-    }
-
-    private void sendSMS(String phoneNumber, String message) {
-
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
-
-        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
-
-        // ---when the SMS has been sent---
-        registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS sent",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        Toast.makeText(getBaseContext(), "Generic failure",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        Toast.makeText(getBaseContext(), "No service",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        Toast.makeText(getBaseContext(), "Null PDU",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-
-                        Toast.makeText(getBaseContext(), "Radio off",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(SENT));
-
-        /*
-
-        // ---when the SMS has been delivered---
-        registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode()) {
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getBaseContext(), "SMS delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Toast.makeText(getBaseContext(), "SMS not delivered",
-                                Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter(DELIVERED));
-
-        */
-
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
-
-
-
-    }
-
-    //not sure if we need this but in theory we need to also close the receiver, maybe in the main activity.
-//    @Override
-//    protected void onDestroy() {
-//        unregisterReceiver(receiver);
-//        super.onDestroy();
+//
+//    private void sendSMS(String phoneNumber, String message) {
+//
+//        String SENT = "SMS_SENT";
+//        String DELIVERED = "SMS_DELIVERED";
+//
+//        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+//        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+//
+//        // ---when the SMS has been sent---
+//        registerReceiver(new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context arg0, Intent arg1) {
+//                switch (getResultCode()) {
+//                    case Activity.RESULT_OK:
+//                        Toast.makeText(getBaseContext(), "SMS sent",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+//                        Toast.makeText(getBaseContext(), "Generic failure",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+//                        Toast.makeText(getBaseContext(), "No service",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case SmsManager.RESULT_ERROR_NULL_PDU:
+//                        Toast.makeText(getBaseContext(), "Null PDU",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+//
+//                        Toast.makeText(getBaseContext(), "Radio off",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                }
+//            }
+//        }, new IntentFilter(SENT));
+//
+//        /*
+//
+//        // ---when the SMS has been delivered---
+//        registerReceiver(new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context arg0, Intent arg1) {
+//                switch (getResultCode()) {
+//                    case Activity.RESULT_OK:
+//                        Toast.makeText(getBaseContext(), "SMS delivered",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                    case Activity.RESULT_CANCELED:
+//                        Toast.makeText(getBaseContext(), "SMS not delivered",
+//                                Toast.LENGTH_SHORT).show();
+//                        break;
+//                }
+//            }
+//        }, new IntentFilter(DELIVERED));
+//
+//        */
+//
+//        SmsManager sms = SmsManager.getDefault();
+//        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
 //    }
-
-}
