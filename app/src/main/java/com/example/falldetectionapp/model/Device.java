823ce35aa7e65224e6cb79f50acd6fae4760dd0c
Victornrib <victornrib@gmail.com>
2022-05-17 16:58:05 +0200

    Bluetooth Connection receiving signal (with crash)

--- app/src/main/java/com/example/falldetectionapp/model/Device.java
+++ app/src/main/java/com/example/falldetectionapp/model/Device.java
@@ -3,11 +3,9 @@ package com.example.falldetectionapp.model;
 //Do we need this class?
 
 public class Device {
-    int userID; //should device have unique ID?
-    String wifiPassword;
-    String wifiSSID;
-    boolean isConnected;
-    boolean fallDetected;
+    //String DeviceName
+    //String MAC_ADDRESS
+
 
     public Device(){    //parameters missing?
 
